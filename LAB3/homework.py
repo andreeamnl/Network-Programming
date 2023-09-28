169bcaf3be5d5c1642d2c2351f8002bad711de2b
OzzyOsbourne <andreeamanole2020@gmail.com>
2023-09-28 22:13:14 +0300

    Add web crawler

---
+++ LAB3/homework.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import json  
+lnk = "https://999.md/ro/84159408"  #this is a real scrapped link
+lnk = "https://999.md/ro/84349867"   #this other link simply has more data to be scrapped
+ 
+ 
+def tojson(lnk):
+    reqs = requests.get(lnk)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+
+    prop_list = soup.find_all('li',class_="m-value")
+    properties = {}
+    for elem in prop_list:   #iterates throuhgh all properties and adds them to a dictionary
+        elem = elem.get_text().split()
+        properties[elem[0]] = elem[1]
+
+    for link in soup.find_all('a'):   #finds first phone number
+        href = link.get('href')
+        if href is not None and ("+373" in href):
+            tel = (link.get('href')[5:16])
+            break
+
+    default = {
+        "Name" : soup.find('header',class_="adPage__header").get_text(),
+        "Description" : soup.find('div',class_="adPage__content__description grid_18").get_text(),
+        "Price" : soup.find('span',class_="adPage__content__price-feature__prices__price__value").get_text() + soup.find('span',class_="adPage__content__price-feature__prices__price__currency").get_text(), 
+        "Country" : soup.find('dd',itemprop="address").get_text(),
+        "Phone" : tel,
+    }
+    default.update(properties) ## adds up both dictionaries
+    y = json.dumps(default, ensure_ascii=False)
+    return y
+
+
+
+print(tojson(lnk))
+
+                
---
+++ LAB3/in_class.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+urls = ["https://999.md/ro/list/animals-and-plants/cats"]
+data = []
+
+
+def fetch_and_parse(current, urls, data, max):
+    current_link = urls[current]
+    page = requests.get(current_link)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    for link in soup.find_all('a',class_='js-item-ad'):
+        link = str(link.get('href'))
+        if "booster" not in link:
+            url = 'https://999.md' + link
+            data.append(url)
+
+    max_pages = soup.select('nav.paginator > ul > li > a')
+    for page in max_pages:
+        link = str('https://999.md' + page['href'])
+        if link not in urls:
+            urls.append(link)
+    try:
+                
+        if current == max or current >= len(urls)-1:
+            return data
+        else:
+            
+            fetch_and_parse(current+1, urls, data, max)
+    except Exception as e:
+        print(e)
+
+init = fetch_and_parse(0, urls, data, 10)
+
+
+f = open("links.txt", "a")
+for link in data:
+    f.write(link + "\n")
+    #print(link)
+f.close()
+
+#print(data)
