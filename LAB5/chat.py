89d56a0e085736afc34d3b10f43f8e3b6517140a
OzzyOsbourne <andreeamanole2020@gmail.com>
2023-10-12 17:07:08 +0300

    fix

--- LAB5/chat.py
+++ LAB5/chat.py
@@ -1,66 +1,186 @@
 import socket
 import threading
 import json
+import os
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 1111
+MEDIA_FOLDER = 'server_media'
+CLIENTS = {}
+ROOMS = {}
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
+def start_server():
+    server_socket = create_server_socket()
+    accept_connections(server_socket)
 
-server_rooms = {}
-server_clients = []
+def create_server_socket():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    print(f'Server is listening on {HOST}:{PORT}')
+    return server_socket
 
+def accept_connections(server_socket):
+    while True:
+        client_socket, client_address = server_socket.accept()
+        CLIENTS[client_socket] = {'address': client_address}
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
 
-def client_handler(client_socket, client_address):
-
-    print(f'Connection from {client_address}')
+def handle_client(client_socket):
+    client_address = CLIENTS[client_socket]['address']
+    print(f'Accepted connection from {client_address}')
 
     while True:
-        client_message = client_socket.recv(1024).decode('utf-8')
-
-        if not client_message:
+        message = client_socket.recv(1024).decode('utf-8')
+        if not message:
             break
 
-        print(f'Message: {client_message}, from {client_address} ')
+        data = json.loads(message)
+        handle_message(client_socket, data)
 
-        message = json.loads(client_message)
+def handle_message(client_socket, data):
+    message_type = data.get('type', '')
 
-        if message['type'] == 'connect':
-            room_name = message['payload']['room']
-            if room_name not in server_rooms:
-                server_rooms[room_name] = [client_socket]
-            else:
-                server_rooms[room_name].append(client_socket)
+    if message_type == 'connect':
+        handle_connect(client_socket, data)
+    elif message_type == 'disconnect':
+        handle_disconnect(client_socket, data)
+    elif message_type == 'upload':
+        handle_upload(client_socket, data)
+    elif message_type == 'download':
+        handle_download(client_socket, data)
+    elif message_type == 'message':
+        handle_message_broadcast(client_socket, data)
+    else:
+        print(f'\nInvalid client message received: {data}')
 
-            for client in server_rooms[message['payload']['room']]:
-                resp = {
-                    "type": "connection",
-                    "payload": {
-                        "message": f"Client {message['payload']['name']} has been connected "
-                                   f"to the {message['payload']['room']}."
-                    }
-                }
-                client.send(json.dumps(resp).encode('utf-8'))
-        elif message['type'] == 'message':
-            for client in server_rooms[message['payload']['room']]:
-                notification = {
-                    "type": "notification",
-                    "payload": {
-                        "text": message['payload']['text']
-                    }
-                }
+def handle_connect(client_socket, data):
+    acknowledge_message = {
+        "type": "connect_ack",
+        "payload": {
+            "message": f"\nYou successfully connected to the room '{data['payload']['room']}'."
+        }
+    }
+    server_data = json.dumps(acknowledge_message)
+    client_socket.send(bytes(server_data, encoding='utf-8'))
 
-                client.send(json.dumps(notification).encode('utf-8'))
+    room = data['payload']['room']
+    if room not in ROOMS:
+        ROOMS[room] = set()
 
-    server_clients.remove(client_socket)
-    client_socket.close()
+    ROOMS[room].add(client_socket)
 
+    if len(CLIENTS) > 1:
+        notification_message = {
+            "type": "notification",
+            "payload": {
+                "message": f"{data['payload']['name']} has joined the room.\n"
+            }
+        }
+        server_data = json.dumps(notification_message)
+        send_broadcast_message(client_socket, CLIENTS, ROOMS, bytes(server_data, encoding='utf-8'))
 
-while True:
-    client_socket, client_address = server_socket.accept()
-    server_clients.append(client_socket)
-    client_thread = threading.Thread(target=client_handler, args=(client_socket, client_address))
-    client_thread.start()
+def handle_disconnect(client_socket, data):
+    CLIENTS.pop(client_socket, None)
 
+    if len(CLIENTS) != 0:
+        notification_message = {
+            "type": "notification",
+            "payload": {
+                "message": f"{data['payload']['name']} left the room.\n"
+            }
+        }
+        server_data = json.dumps(notification_message)
+        send_broadcast_message(client_socket, CLIENTS, ROOMS, bytes(server_data, encoding='utf-8'))
+
+    for room in ROOMS:
+        if room == data['payload']['room']:
+            ROOMS[room].discard(client_socket)
+            break
+
+def handle_upload(client_socket, data):
+    room = data['payload']['room']
+    file_name = data['payload']['file_name']
+    file_size = data['payload']['file_size']
+    file_path = os.path.join(MEDIA_FOLDER, room, file_name)
+
+    if not os.path.isdir(os.path.join(MEDIA_FOLDER, room)):
+        os.makedirs(os.path.join(MEDIA_FOLDER, room))
+
+    with open(file_path, 'wb') as file:
+        while file_size > 0:
+            chunk = client_socket.recv(1024)
+            if not chunk:
+                break
+            file.write(chunk)
+            file_size -= len(chunk)
+
+    notification_message = {
+        "type": "notification",
+        "payload": {
+            "message": f"{data['payload']['name']} uploaded the {file_name} file.\n"
+        }
+    }
+    server_data = json.dumps(notification_message)
+    send_broadcast_message(client_socket, CLIENTS, ROOMS, bytes(server_data, encoding='utf-8'))
+
+def handle_download(client_socket, data):
+    room = data['payload']['room']
+    file_name = data['payload']['file_name']
+    file_path = os.path.join(MEDIA_FOLDER, room, file_name)
+
+    if os.path.exists(file_path):
+        file_size = os.path.getsize(file_path)
+
+        stream_message = {
+            "type": "download-ack",
+            "payload": {
+                "file_name": file_name,
+                "file_size": file_size
+            }
+        }
+        server_data = json.dumps(stream_message)
+        client_socket.sendall(bytes(server_data, encoding='utf-8'))
+
+        with open(file_path, 'rb') as file:
+            for chunk in iter(lambda: file.read(1024), b''):
+                client_socket.sendall(chunk)
+    else:
+        notification_message = {
+            "type": "notification",
+            "payload": {
+                "message": f"The file {file_name} does not exist.\n"
+            }
+        }
+        server_data = json.dumps(notification_message)
+        client_socket.sendall(bytes(server_data, encoding='utf-8'))
+
+def handle_message_broadcast(client_socket, data):
+    room = data['payload']['room']
+    sender = data['payload']['sender']
+    message_text = data['payload']['text']
+
+    message_to_broadcast = {
+        "type": "message",
+        "payload": {
+            "room": room,
+            "sender": sender,
+            "text": message_text
+        }
+    }
+
+    server_data = json.dumps(message_to_broadcast)
+    send_broadcast_message(client_socket, CLIENTS, ROOMS, bytes(server_data, encoding='utf-8'))
+
+def send_broadcast_message(client_socket, clients, rooms, data):
+    for client in clients:
+        if client != client_socket:
+            for room in rooms:
+                if (client in rooms[room]) and (client_socket in rooms[room]):
+                    client.sendall(data)
+                    break
+
+if __name__ == '__main__':
+    start_server()
--- LAB5/client.py
+++ LAB5/client.py
@@ -1,66 +1,181 @@
 import socket
 import threading
 import json
+import os
+import re
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 1111
+MEDIA_FOLDER = 'client_media'
+CLIENT_SOCKET = None
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-client_socket.connect((HOST, PORT))
+def connect():
+    global CLIENT_SOCKET
+    CLIENT_SOCKET = create_client_socket()
+    print(f"Connected to {HOST}:{PORT}")
+    
 
+def create_client_socket():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client_socket.connect((HOST, PORT))
+    return client_socket
 
-def message_handler():
+def perform_server_connection():
+    name = input('Enter your name: ')
+    room = input('Enter the room: ')
 
-    while True:
-        message = client_socket.recv(1024).decode('utf-8')
-
-        if not message:
-            break
-
-        message = json.loads(message)
-
-        if message['type'] == 'connection':
-            print(f"Connection received: {message['payload']['message']}")
-
-        elif message['type'] == 'notification':
-            print(f"Message received: {message['payload']['text']}")
-
-
-client_thread = threading.Thread(target=message_handler)
-client_thread.daemon = True
-client_thread.start()
-
-connection_room = str(input('Room: '))
-client_name = str(input('Name: '))
-
-message = {
-    "type": "connect",
-    "payload": {
-        "room": connection_room,
-        "name": client_name
-    }
-}
-
-
-client_socket.send((json.dumps(message)).encode('utf-8'))
-
-while True:
-
-    client_message = str(input())
-
-    if client_message.upper() == 'EXIT':
-        break
-
-    mess = {
-        "type": "message",
+    connection_message = {
+        "type": "connect",
         "payload": {
-            "room": connection_room,
-            "text": client_message,
-            "sender": client_name
+            "name": name,
+            "room": room
         }
     }
 
-    client_socket.send(json.dumps(mess).encode('utf-8'))
+    data = json.dumps(connection_message)
+    CLIENT_SOCKET.sendall(bytes(data, encoding='utf-8'))
 
-client_socket.close()
+    return name, room
+
+def receive_messages(name):
+    while True:
+        server_message = CLIENT_SOCKET.recv(1024).decode('utf-8')
+        if not server_message:
+            break
+
+        try:
+            data = json.loads(server_message)
+        except json.JSONDecodeError:
+            print("Received invalid JSON data from the server.")
+            continue
+
+        handle_server_message(data, name)
+
+
+def handle_server_message(data, name):
+    message_type = data.get('type', '')
+
+    if message_type == 'connect_ack' or message_type == 'notification':
+        print(data['payload']['message'])
+    elif message_type == 'download-ack':
+        download_file(data, name)
+    elif message_type == 'message':
+        print(f"\nRoom: {data['payload']['room']}, {data['payload']['sender']}: {data['payload']['text']}")
+    else:
+        print(f'\nInvalid message received: {data}')
+
+def upload_file(file_path, name, room):
+    file_name = os.path.basename(file_path)
+    file_size = os.path.getsize(file_path)
+    file_upload_message = {
+        "type": "upload",
+        "payload": {
+            "file_name": file_name,
+            "file_size": file_size,
+            "name": name,
+            "room": room
+        }
+    }
+    data = json.dumps(file_upload_message)
+    CLIENT_SOCKET.sendall(bytes(data, encoding='utf-8'))
+
+    with open(file_path, 'rb') as file:
+        while True:
+            chunk = file.read(1024)
+            if not chunk:
+                break
+            CLIENT_SOCKET.sendall(chunk)
+
+def download_file(data, name):
+    message_type = data.get('type', '')
+
+    if message_type == 'download-ack':
+        file_name = data['payload']['file_name']
+
+        room = data['payload'].get('room', '')  
+
+        file_path = os.path.join(MEDIA_FOLDER, room, file_name)
+
+        if os.path.exists(file_path):
+            file_size = os.path.getsize(file_path)
+
+            stream_message = {
+                "type": "download-ack",
+                "payload": {
+                    "file_name": file_name,
+                    "file_size": file_size
+                }
+            }
+            server_data = json.dumps(stream_message)
+            CLIENT_SOCKET.sendall(bytes(server_data, encoding='utf-8'))
+
+            with open(file_path, 'rb') as file:
+                for chunk in iter(lambda: file.read(1024), b''):
+                    CLIENT_SOCKET.sendall(chunk)
+        else:
+            notification_message = {
+                "type": "notification",
+                "payload": {
+                    "message": f"The file {file_name} does not exist.\n"
+                }
+            }
+            server_data = json.dumps(notification_message)
+            CLIENT_SOCKET.sendall(bytes(server_data, encoding='utf-8'))
+    else:
+        print(f'Invalid message type for download: {message_type}')
+
+
+def main():
+    connect()
+    name, room = perform_server_connection()
+
+    if not os.path.isdir(os.path.join(MEDIA_FOLDER, name)):
+        os.makedirs(os.path.join(MEDIA_FOLDER, name))
+
+    receive_thread = threading.Thread(target=receive_messages, args=(name,))
+    receive_thread.daemon = True
+    receive_thread.start()
+
+    while True:
+        message = input()
+        if message.lower() == 'exit':
+            disconnect_message = {
+                "type": "disconnect",
+                "payload": {
+                    "name": name,
+                    "room": room
+                }
+            }
+            data = json.dumps(disconnect_message)
+            CLIENT_SOCKET.sendall(bytes(data, encoding='utf-8'))
+            break
+
+        if re.match(r'upload ([A-Za-z\./]+)', message):
+            upload_file(message.split(' ')[1], name, room)
+        elif re.match(r'download ([A-Za-z\.]+)', message):
+            file_download_message = {
+                "type": "download",
+                "payload": {
+                    "file_name": message.split(' ')[1],
+                    "name": name,
+                    "room": room
+                }
+            }
+            data = json.dumps(file_download_message)
+            CLIENT_SOCKET.sendall(bytes(data, encoding='utf-8'))
+        else:
+            chat_message = {
+                "type": "message",
+                "payload": {
+                    "sender": name,
+                    "room": room,
+                    "text": f'{message}\n'
+                }
+            }
+
+            data = json.dumps(chat_message)
+            CLIENT_SOCKET.sendall(bytes(data, encoding='utf-8'))
+
+if __name__ == '__main__':
+    main()
